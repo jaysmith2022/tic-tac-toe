26ba6003a728de6dc32415c8e73e6dc5e8dc558c
jordan smith <jordan.smith6035@gmail.com>
2022-11-08 21:47:38 -0700

    Feat/add css buildout

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 video = document.getElementById("player2-idle");
-video.playbackRate = 2.0;
+video.playbackRate = 1.3;
 video = document.getElementById("player1-idle");
-video.playbackRate = 2.0;
+video.playbackRate = 1.5;
